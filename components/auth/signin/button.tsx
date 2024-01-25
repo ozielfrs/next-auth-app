5123ffc07ef244823aa003d5f83086a86f7d9b17
Oziel Ferreira <oziel@unifei.edu.br>
2024-01-25 19:19:04 -0300

    created base for signing in

---
+++ components/auth/signin/button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface SignInButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "page";
+  asChild?: boolean;
+}
+
+const SignInButton = ({
+  children,
+  mode = "page",
+  asChild,
+}: SignInButtonProps) => {
+  const router = useRouter();
+
+  const clickHandler = () => {
+    router.push(`/signin`);
+  };
+
+  return (
+    <span onClick={clickHandler} className={"cursor-pointer"}>
+      {children}
+    </span>
+  );
+};
+
+export default SignInButton;
